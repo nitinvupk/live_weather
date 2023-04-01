6cdf6ebf5a405b75acc8939bc8870247d61486e4
Nitin Verma <nitinvupk83@gmail.com>
2023-04-01 19:55:10 +0530

    Added pages and UI Fixes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+require 'net/http'
+require 'json'
+
+class PagesController < ApplicationController
+  def home
+    @city = City.new
+    @cities = City.all
+  end
+
+  def help
+  end
+end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,42 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
 class City < ApplicationRecord
+  before_save   :downcase_name
+  before_create :add_timezone
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, 
+                   length: { maximum: 50 }
+
+  class << self
+    # Retrieves the weather json
+    def get_weather(name, type="weather")
+      api_key = 'affd39b1038eeab8063b783a52f87dad'
+
+      # TODO: Change units based on locale
+      uri = URI("http://api.openweathermap.org/data/2.5/#{type}?q=#{CGI::escape(name)}&units=imperial&appid=#{api_key}")
+
+      # Download and parse the JSON from the api
+      res = JSON.parse(Net::HTTP.get(uri))
+    end
+  end
+
+  private
+
+  # Converts city name to all lower-case.
+  def downcase_name
+    name.downcase!
+  end
+
+  # Lookup the correct timezone based on the latitude and longitude.
+  def add_timezone
+    res = City.get_weather(name)
+
+    begin
+      self.timezone = Timezone.lookup(res['coord']['lat'], res['coord']['lon']).name
+    rescue Timezone::Error::Base => e
+      puts "Timezone Error: #{e.message}"
+    end
+  end
+
 end
